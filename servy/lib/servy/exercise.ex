376373f3b17adeb0b58af90381366a3a7e263866
Gerson Noboa <gerson.noboa@glia.com>
2022-01-02 16:44:48 +0200

    Up until video 14

---
+++ servy/lib/servy/exercise.ex
@@ -0,0 +1,24 @@
+defmodule Recurse do
+  def loopy([head | tail]) do
+    IO.puts "Head: #{head} Tail: #{inspect(tail)}"
+    loopy(tail)
+  end
+
+  def loopy([]), do: IO.puts "Done!"
+
+	def sum([head | tail], total) do
+		sum(tail, total + head)
+	end
+
+	def sum([], total), do: IO.puts "Result is #{total}"
+
+	def triple([head | tail]) do
+		[head*3 | triple(tail)]
+	end
+
+	def triple([]), do: []
+end
+
+# Recurse.loopy([1, 2, 3, 4, 5])
+# Recurse.sum([1, 2, 3, 4, 5], 0)
+IO.inspect Recurse.triple([1, 2, 3, 4, 5])
