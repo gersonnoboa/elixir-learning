7283f5c5184cb71afd02d6db22b1c63a28a3a00c
Gerson Noboa <gnoboa@protonmail.com>
2022-01-05 09:02:40 +0200

    Up until video 16

--- servy/lib/servy/exercise.ex
+++ servy/lib/servy/exercise.ex
@@ -1,24 +1,31 @@
 defmodule Recurse do
   def loopy([head | tail]) do
-    IO.puts "Head: #{head} Tail: #{inspect(tail)}"
+    IO.puts("Head: #{head} Tail: #{inspect(tail)}")
     loopy(tail)
   end
 
-  def loopy([]), do: IO.puts "Done!"
+  def loopy([]), do: IO.puts("Done!")
 
-	def sum([head | tail], total) do
-		sum(tail, total + head)
-	end
+  def sum([head | tail], total) do
+    sum(tail, total + head)
+  end
 
-	def sum([], total), do: IO.puts "Result is #{total}"
+  def sum([], total), do: IO.puts("Result is #{total}")
 
-	def triple([head | tail]) do
-		[head*3 | triple(tail)]
-	end
+  def triple([head | tail]) do
+    [head * 3 | triple(tail)]
+  end
 
-	def triple([]), do: []
+  def triple([]), do: []
+
+  def my_map([head | tail], function) do
+    [function.(head) | my_map(tail, function)]
+  end
+
+  def my_map([], _function), do: []
 end
 
 # Recurse.loopy([1, 2, 3, 4, 5])
 # Recurse.sum([1, 2, 3, 4, 5], 0)
 # IO.inspect Recurse.triple([1, 2, 3, 4, 5])
+IO.inspect(Recurse.my_map([1, 2, 3], &(&1 * 3)))
--- servy/lib/servy/handler.ex
+++ servy/lib/servy/handler.ex
@@ -1,110 +1,111 @@
 defmodule Servy.Handler do
+  @moduledoc """
+  Handles HTTP requests.
+  """
 
-	@moduledoc """
-	Handles HTTP requests.
-	"""
+  alias Servy.Conv
+  alias Servy.BearController
 
-	alias Servy.Conv
-	alias Servy.BearController
+  @pages_path Path.expand("pages", File.cwd!())
 
-	@pages_path Path.expand("pages", File.cwd!)
+  import Servy.Plugins, only: [rewrite_path: 1, log: 1, track: 1]
+  import Servy.Parser, only: [parse: 1]
+  import Servy.FileHandler, only: [handle_file: 2]
 
-	import Servy.Plugins, only: [rewrite_path: 1, log: 1, track: 1]
-	import Servy.Parser, only: [parse: 1]
-	import Servy.FileHandler, only: [handle_file: 2]
+  @doc """
+  Transforms the request into a response.
+  """
+  def handle(request) do
+    request
+    |> parse
+    |> rewrite_path
+    |> log
+    |> route
+    |> track
+    |> format_response
+  end
 
-	@doc """
-	Transforms the request into a response.
-	"""
-	def handle(request) do
-		request
-		|> parse
-		|> rewrite_path
-		|> log
-		|> route
-		|> track
-		|> format_response
-	end
+  def route(%Conv{method: "GET", path: "/wildthings"} = conv) do
+    %{conv | status: 200, resp_body: "Bears, Lions, Tigers"}
+  end
 
-	def route(%Conv{ method: "GET", path: "/wildthings"} = conv) do
-		%{ conv | status: 200, resp_body: "Bears, Lions, Tigers" }
-	end
+  def route(%Conv{method: "GET", path: "/bears"} = conv) do
+    BearController.index(conv)
+  end
 
-	def route(%Conv{ method: "GET", path: "/bears"} = conv) do
-		BearController.index(conv)
-	end
+  def route(%Conv{method: "GET", path: "/bears/" <> id} = conv) do
+    params = Map.put(conv.params, "id", id)
+    BearController.show(conv, params)
+  end
 
-	def route(%Conv{ method: "GET", path: "/bears/" <> id} = conv) do
-		params = Map.put(conv.params, "id", id)
-		BearController.show(conv, params)
-	end
+  def route(%Conv{method: "GET", path: "/about"} = conv) do
+    @pages_path
+    |> Path.join("about.html")
+    |> File.read()
+    |> handle_file(conv)
+  end
 
-	def route(%Conv{ method: "GET", path: "/about"} = conv) do
-		@pages_path
-		|> Path.join("about.html")
-		|> File.read
-		|> handle_file(conv)
-	end
+  def route(%Conv{method: "POST", path: "/bears"} = conv) do
+    BearController.create(conv, conv.params)
+  end
 
-	def route(%Conv{ method: "POST", path: "/bears" } = conv) do
-		BearController.create(conv, conv.params)
-	end
+  def route(%Conv{method: "DELETE", path: "/bears/" <> id} = conv) do
+    params = Map.put(conv.params, "id", id)
+    BearController.delete(conv, params)
+  end
 
-	def route(%Conv{ method: "DELETE", path: "/bears/" <> _id } = conv) do
-		%{ conv | status: 403, resp_body: "Deleting a bear is forbidden"}
-	end
+  def route(%Conv{path: path} = conv) do
+    %{conv | status: 404, resp_body: "No #{path} here"}
+  end
 
-	def route(%Conv{ path: path} = conv) do
-		%{ conv | status: 404, resp_body: "No #{path} here" }
-	end
+  def format_response(%Conv{} = conv) do
+    """
+    HTTP/1.1 #{Conv.full_status(conv)}
+    Content-Type: text/html
+    Content-Length: #{String.length(conv.resp_body)}
 
-	def format_response(%Conv{} = conv) do
-		"""
-		HTTP/1.1 #{Conv.full_status(conv)}
-		Content-Type: text/html
-		Content-Length: #{ String.length(conv.resp_body) }
+    #{conv.resp_body}
+    """
+  end
 
-		#{ conv.resp_body }
-		"""
-	end
+  def execute_request(method, path) do
+    request = """
+    #{method} #{path} HTTP/1.1
+    Host: example.com
+    User-Agent: ExampleBrowser/1.0
+    Accept: */*
 
-	def execute_request(method, path) do
-		request = """
-		#{method} #{path} HTTP/1.1
-		Host: example.com
-		User-Agent: ExampleBrowser/1.0
-		Accept: */*
+    """
 
-		"""
+    response = Servy.Handler.handle(request)
 
-		response = Servy.Handler.handle(request)
+    IO.puts(response)
+  end
 
-		IO.puts response
-	end
+  def execute_post() do
+    request = """
+    POST /bears HTTP/1.1
+    Host: example.com
+    User-Agent: ExampleBrowser/1.0
+    Accept: */*
+    Content-Type: application/x-www-form-urlencoded
+    Content-Length: 21
 
-	def execute_post() do
-		request = """
-		POST /bears HTTP/1.1
-		Host: example.com
-		User-Agent: ExampleBrowser/1.0
-		Accept: */*
-		Content-Type: application/x-www-form-urlencoded
-		Content-Length: 21
+    name=Baloo&type=Brown
+    """
 
-		name=Baloo&type=Brown
-		"""
+    response = Servy.Handler.handle(request)
 
-		response = Servy.Handler.handle(request)
-
-		IO.puts response
-	end
+    IO.puts(response)
+  end
 end
 
 # Servy.Handler.execute_request("GET", "/wildthings")
- Servy.Handler.execute_request("GET", "/bears")
+Servy.Handler.execute_request("GET", "/bears")
 # Servy.Handler.execute_request("GET", "/bigfoot")
- Servy.Handler.execute_request("GET", "/bears/1")
+Servy.Handler.execute_request("GET", "/bears/1")
 # Servy.Handler.execute_request("GET", "/wildlife")
 # Servy.Handler.execute_request("GET", "/about")
 # Servy.Handler.execute_request("GET", "/bears?id=1")
- Servy.Handler.execute_post()
+# Servy.Handler.execute_request("DELETE", "/bears/1")
+# Servy.Handler.execute_post()
