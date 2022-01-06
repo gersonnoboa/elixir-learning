0d12dc210d41c08b59fcf0950b32b56253e141c6
Gerson Noboa <gnoboa@protonmail.com>
2022-01-06 22:24:02 +0200

    Up until video 18

--- servy/lib/servy/handler.ex
+++ servy/lib/servy/handler.ex
@@ -55,57 +55,16 @@ defmodule Servy.Handler do
   end
 
   def route(%Conv{path: path} = conv) do
-    %{conv | status: 404, resp_body: "No #{path} here"}
+    %{conv | status: 404, resp_body: "No #{path} here!"}
   end
 
   def format_response(%Conv{} = conv) do
     """
-    HTTP/1.1 #{Conv.full_status(conv)}
-    Content-Type: text/html
-    Content-Length: #{String.length(conv.resp_body)}
-
+    HTTP/1.1 #{Conv.full_status(conv)}\r
+    Content-Type: text/html\r
+    Content-Length: #{String.length(conv.resp_body)}\r
+    \r
     #{conv.resp_body}
     """
   end
-
-  def execute_request(method, path) do
-    request = """
-    #{method} #{path} HTTP/1.1
-    Host: example.com
-    User-Agent: ExampleBrowser/1.0
-    Accept: */*
-
-    """
-
-    response = Servy.Handler.handle(request)
-
-    IO.puts(response)
-  end
-
-  def execute_post() do
-    request = """
-    POST /bears HTTP/1.1
-    Host: example.com
-    User-Agent: ExampleBrowser/1.0
-    Accept: */*
-    Content-Type: application/x-www-form-urlencoded
-    Content-Length: 21
-
-    name=Baloo&type=Brown
-    """
-
-    response = Servy.Handler.handle(request)
-
-    IO.puts(response)
-  end
 end
-
-# Servy.Handler.execute_request("GET", "/wildthings")
-Servy.Handler.execute_request("GET", "/bears")
-# Servy.Handler.execute_request("GET", "/bigfoot")
-Servy.Handler.execute_request("GET", "/bears/1")
-# Servy.Handler.execute_request("GET", "/wildlife")
-# Servy.Handler.execute_request("GET", "/about")
-# Servy.Handler.execute_request("GET", "/bears?id=1")
-# Servy.Handler.execute_request("DELETE", "/bears/1")
-# Servy.Handler.execute_post()
--- servy/lib/servy/plugins.ex
+++ servy/lib/servy/plugins.ex
@@ -1,36 +1,42 @@
 defmodule Servy.Plugins do
+  alias Servy.Conv
 
-	alias Servy.Conv
+  @doc """
+  Logs 404 requests.
+  """
+  def track(%Conv{status: 404, path: path} = conv) do
+    if Mix.env() != :test do
+      IO.puts("Warning: #{path} is on the loose!")
+    end
 
-	@doc """
-	Logs 404 requests.
-	"""
-	def track(%Conv{status: 404, path: path} = conv) do
-		IO.puts "Warning: #{path} is on the loose!"
-		conv
-	end
+    conv
+  end
 
-	def track(%Conv{} = conv), do: conv
+  def track(%Conv{} = conv), do: conv
 
-	def log(conv) do
-		IO.inspect conv
-	end
+  def log(conv) do
+    if Mix.env() == :dev do
+      IO.inspect(conv)
+    end
 
-	def rewrite_path(%Conv{path: "/wildlife"} = conv) do
-		%{ conv | path: "/wildthings" }
-	end
+    conv
+  end
 
-	def rewrite_path(%Conv{path: path} = conv) do
-		regex = ~r{\/(?<thing>\w+)\?id=(?<id>\d+)}
-		captures = Regex.named_captures(regex, path)
-		rewrite_path_captures(conv, captures)
-	end
+  def rewrite_path(%Conv{path: "/wildlife"} = conv) do
+    %{conv | path: "/wildthings"}
+  end
 
-	def rewrite_path(%Conv{} = conv, nil), do: conv
+  def rewrite_path(%Conv{path: path} = conv) do
+    regex = ~r{\/(?<thing>\w+)\?id=(?<id>\d+)}
+    captures = Regex.named_captures(regex, path)
+    rewrite_path_captures(conv, captures)
+  end
 
-	def rewrite_path_captures(%Conv{} = conv, %{"thing" => thing, "id" => id}) do
-		%{ conv | path: "/#{thing}/#{id}"}
-	end
+  def rewrite_path(%Conv{} = conv, nil), do: conv
 
-	def rewrite_path_captures(%Conv{} = conv, nil), do: conv
+  def rewrite_path_captures(%Conv{} = conv, %{"thing" => thing, "id" => id}) do
+    %{conv | path: "/#{thing}/#{id}"}
+  end
+
+  def rewrite_path_captures(%Conv{} = conv, nil), do: conv
 end
