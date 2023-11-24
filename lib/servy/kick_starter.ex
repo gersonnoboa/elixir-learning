5a4da78b8a6888e51e214d7772275a675f09bac9
Gerson Noboa <gerson.noboa@glia.com>
2023-11-24 16:05:56 +0200

    Course finished

--- lib/servy/kick_starter.ex
+++ lib/servy/kick_starter.ex
@@ -1,9 +1,9 @@
 defmodule Servy.KickStarter do
   use GenServer
 
-  def start do
+  def start_link(_arg) do
     IO.puts("Starting the kockstarter...")
-    GenServer.start(__MODULE__, :ok, name: __MODULE__)
+    GenServer.start_link(__MODULE__, :ok, name: __MODULE__)
   end
 
   def init(:ok) do
@@ -13,14 +13,15 @@ defmodule Servy.KickStarter do
   end
 
   def handle_info({:EXIT, _pid, reason}, _state) do
-    IO.puts("HttServer exit #{inspect(reason)}")
+    IO.puts("HttpServer exit #{inspect(reason)}")
     server_pid = start_server()
     {:noreply, server_pid}
   end
 
   defp start_server do
     IO.puts("starting http server")
-    server_pid = spawn_link(Servy.HttpServer, :start, [4000])
+    port = Application.get_env(:servy, :port)
+    server_pid = spawn_link(Servy.HttpServer, :start, [port])
     Process.register(server_pid, :http_server)
     server_pid
   end
